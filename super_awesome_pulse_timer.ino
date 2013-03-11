f05762144f02beb1c8d9af5e0ee2c977c75833d3
Andrew Orr <aorr@iders.ca>
2013-03-11 14:56:22 -0500

    More cleaning

--- super_awesome_pulse_timer.ino
+++ super_awesome_pulse_timer.ino
@@ -3,19 +3,23 @@
  */
 
 #include <inc/hw_timer.h>
+#include <inc/hw_ints.h>
 #include <driverlib/timer.h>
 #include <driverlib/sysctl.h>
+#include <driverlib/interrupt.h>
 
 unsigned long g_last_measured_time = 0;
  
-void timer_a_interrupt(void) {
-  // timer a interrupt
+void timer_0_interrupt(void) {
+  // timer 0 interrupt
   
   // save the timer config so we know which edge this is
   unsigned long timer_event_reg = HWREG(TIMER0_BASE + TIMER_O_CTL);
   
   if ((timer_event_reg & TIMER_EVENT_POS_EDGE) == TIMER_EVENT_POS_EDGE) {
     // rising edge!
+    // clear int mask
+    TimerIntClear(TIMER0_BASE, TIMER_CAPA_EVENT);
     
     // reset value
     TimerLoadSet(TIMER0_BASE, TIMER_A, 0);
@@ -25,6 +29,8 @@ void timer_a_interrupt(void) {
     
   } else if ((timer_event_reg & TIMER_EVENT_NEG_EDGE) == TIMER_EVENT_NEG_EDGE) {
     // falling edge!
+    // clear int mask
+    TimerIntClear(TIMER0_BASE, TIMER_CAPA_EVENT);
     
     // save our measure ticks
     g_last_measured_time =  TimerValueGet(TIMER0_BASE, TIMER_A);
@@ -58,10 +64,16 @@ void setup() {
   TimerLoadSet(TIMER0_BASE, TIMER_A, 0);
 
   // register our interrupt 
-  TimerIntRegister(TIMER0_BASE, TIMER_A, timer_a_interrupt);
+  TimerIntRegister(TIMER0_BASE, TIMER_A, timer_0_interrupt);
   
   // interrupt on timer a capture event
   TimerIntEnable(TIMER0_BASE, TIMER_CAPA_EVENT);
+  
+  // enable
+  IntEnable(INT_TIMER0A);
+  
+  // enable
+  IntMasterEnable();
   
   // and turn it on
   TimerEnable(TIMER0_BASE, TIMER_A);
